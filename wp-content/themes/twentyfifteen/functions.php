0cf82fc5441ad9ab0c05bf4c4d505592f1e144fd
Cowart <cowart.suazo@quantumlinx.com.au>
2017-08-15 12:02:56 +0800

    editing functions.php

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -168,6 +168,20 @@ function twentyfifteen_widgets_init() {
 }
 add_action( 'widgets_init', 'twentyfifteen_widgets_init' );
 
+function sample_cowart_init_ds(){
+	register_sidebar( array(
+		'name'          => __( 'Widget Area', 'twentyfifteen' ),
+		'id'            => 'sidebar-2',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentyfifteen' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+
+add_action('widgets_init', 'sample_cowart_init_ds');
+
 if ( ! function_exists( 'twentyfifteen_fonts_url' ) ) :
 /**
  * Register Google fonts for Twenty Fifteen.
